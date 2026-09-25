85f15d93dc62fafec9d4f4c5175303f28ac045e5
puang59 <kk61468@gmail.com>
2026-09-25 07:53:21 +0530

    chore: drop the unused filesystem plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -277,7 +277,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
